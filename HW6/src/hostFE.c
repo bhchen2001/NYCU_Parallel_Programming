eb4cb25c88792347d8e48e94a9eee5fdafe15b6c
bhchen2001 <bhchen312551074.cs12@nycu.edu.tw>
2024-01-04 10:09:01 +0800

    feat: opencl finish

--- HW6/src/hostFE.c
+++ HW6/src/hostFE.c
@@ -12,44 +12,8 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
     cl_int status;
     int filter_size = filterWidth * filterWidth;
     int img_size = imageHeight * imageWidth;
+    int half_filter_width = filterWidth / 2;
 
-    /* get platfrom ids */
-    // cl_uint num_platforms;
-    // cl_platform_id platform;
-    // if(clGetPlatformIDs(1, &platform, &num_platforms) != CL_SUCCESS)
-    // {
-    //     printf("Error: Failed to find an OpenCL platform!\n");
-    //     exit(1);
-    // }
-
-    /* get the num of GPU */
-    // cl_uint num_devices;
-    // if(clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, NULL, &num_devices) != CL_SUCCESS)
-    // {
-    //     printf("Error: Failed to find an OpenCL device!\n");
-    //     exit(1);
-    // }
-
-    // printf("num_platforms: %d\n", num_platforms);
-    // printf("num_devices: %d\n", num_devices);
-    
-    /* choose GPU device */
-    // if(clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, num_devices, device, NULL) != CL_SUCCESS)
-    // {
-    //     printf("Error: Failed to create a device group!\n");
-    //     exit(1);
-    // }
-
-    /* declare context */
-    // cl_context_properties properties[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)platform, 0};
-    // *context = clCreateContextFromType(properties, CL_DEVICE_TYPE_GPU, NULL, NULL, &status);
-    // if(*context == 0)
-    // {
-    //     printf("Error: Failed to create a compute context!\n");
-    //     exit(1);
-    // }
-    /* choose device from context */
-    // status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, device, NULL);
     /* using device and context to create a command queue */
     cl_command_queue command_queue = clCreateCommandQueue(*context, *device, 0, &status);
 
@@ -62,49 +26,6 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         printf("Error: Failed to allocate device memory!\n");
         exit(1);
     }
-    /* transfer input data to device */
-    // status = clEnqueueWriteBuffer(command_queue, input_buffer, CL_TRUE, 0, img_size * sizeof(float), (void *)inputImage, 0, NULL, NULL);
-    // status = clEnqueueWriteBuffer(command_queue, filter_buffer, CL_TRUE, 0, filter_size * sizeof(float), (void *)filter, 0, NULL, NULL);
-
-    /* read kernel source file */
-    FILE *file;
-    size_t source_size;
-    char *source_str;
-    file = fopen("kernel.cl", "r");
-    if(file == NULL){
-        printf("Failed to load kernel.\n");
-        exit(1);
-    }
-
-    /* get the length of source */
-    // fseek(file, 0, SEEK_END);
-    // source_size = ftell(file);
-    // fseek(file, 0, SEEK_SET);
-
-    // printf("source_size: %d\n", source_size);
-
-    /* read the source file */
-    // source_str = (char *)malloc(source_size + 1); // +1 for '\0'
-    // if(fread(source_str, source_size, 1, file) == 0){
-    //     printf("Failed to read kernel.\n");
-    //     exit(1);
-    // }
-    // fclose(file);
-    // source_str[source_size] = '\0';
-
-    /* create program object */
-    // *program = clCreateProgramWithSource(*context, 1, (const char **)&source_str, (const size_t *)&source_size, &status);
-    // if(*program == NULL){
-    //     printf("Failed to create program.\n");
-    //     exit(1);
-    // }
-
-    /* build program */
-    // status = clBuildProgram(*program, 0, NULL, NULL, NULL, NULL);
-    // if(status != CL_SUCCESS){
-    //     printf("Failed to build program.\n");
-    //     exit(1);
-    // }
 
     /* create kernel */
     cl_kernel kernel = clCreateKernel(*program, "convolution", &status);
@@ -118,29 +39,25 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         printf("Failed to set kernel arguments: filterWidth.\n");
         exit(1);
     }
-    if(clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&filter_buffer) != CL_SUCCESS){
+    if(clSetKernelArg(kernel, 1, sizeof(cl_int), (void *)&half_filter_width) != CL_SUCCESS){
         printf("Failed to set kernel arguments: filter_buffer.\n");
         exit(1);
     }
-    if(clSetKernelArg(kernel, 2, sizeof(cl_int), (void *)&imageHeight) != CL_SUCCESS){
-        printf("Failed to set kernel arguments: imageHeight.\n");
+    if(clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&filter_buffer) != CL_SUCCESS){
+        printf("Failed to set kernel arguments: filter_buffer.\n");
         exit(1);
     }
-    if(clSetKernelArg(kernel, 3, sizeof(cl_int), (void *)&imageWidth) != CL_SUCCESS){
-        printf("Failed to set kernel arguments: imageWidth.\n");
-        exit(1);
-    }
-    if(clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&input_buffer) != CL_SUCCESS){
+    if(clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *)&input_buffer) != CL_SUCCESS){
         printf("Failed to set kernel arguments: input_buffer.\n");
         exit(1);
     }
-    if(clSetKernelArg(kernel, 5, sizeof(cl_mem), (void *)&output_buffer) != CL_SUCCESS){
+    if(clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&output_buffer) != CL_SUCCESS){
         printf("Failed to set kernel arguments: output_buffer.\n");
         exit(1);
     }
 
     /* set workgroup size */
-    size_t local_work_size[2] = {40, 25};
+    size_t local_work_size[2] = {8, 8};
     size_t global_work_size[2] = {imageWidth, imageHeight};
 
     /* execute kernel */
@@ -150,19 +67,19 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
     }
 
     /* read output data */
-    if(clEnqueueReadBuffer(command_queue, output_buffer, CL_TRUE, 0, img_size * sizeof(float), (void *)outputImage, 0, NULL, NULL != CL_SUCCESS)){
+    if(clEnqueueReadBuffer(command_queue, output_buffer, CL_TRUE, 0, img_size * sizeof(float), (void *)outputImage, 0, NULL, NULL) != CL_SUCCESS){
         printf("Failed to read output data.\n");
         exit(1);
     }
 
     /* release resources */
     // status = clReleaseKernel(kernel);
-    // status = clReleaseProgram(*program);
     // status = clReleaseMemObject(input_buffer);
     // status = clReleaseMemObject(filter_buffer);
     // status = clReleaseMemObject(output_buffer);
     // status = clReleaseCommandQueue(command_queue);
     // status = clReleaseContext(*context);
+    // status = clReleaseProgram(*program);
 
     return;
 }
--- HW6/src/kernel.cl
+++ HW6/src/kernel.cl
@@ -1,24 +1,41 @@
-__kernel void convolution(int filterWidth, __global float *filter, int imageHeight, int imageWidth, __global float *inputImage, __global float *outputImage){
+__kernel void convolution(int filterWidth, int halfWidth,  __constant const float *filter, __global const float *inputImage, __global float *outputImage){
     int x = get_global_id(0);
     int y = get_global_id(1);
-    int halfWidth = filterWidth / 2;
+
+    int imageWidth = get_global_size(0);
+    int imageHeight = get_global_size(1);
+    // int halfWidth = filterWidth / 2;
     float sum = 0.0f;
-    // for(int i = -halfWidth; i <= halfWidth; i++){
-    //     for(int j = -halfWidth; j <= halfWidth; j++){
-    //         if(x + i >= 0 && x + i < imageWidth && y + j >= 0 && y + j < imageHeight){
-    //             sum += filter[(i + halfWidth) + filterWidth * (j + halfWidth)] * inputImage[(y + j) * imageWidth + (x + i)];
+
+    int x_img_start = x - halfWidth < 0 ? 0 : x - halfWidth;
+    int x_img_end = x + filterWidth - halfWidth - 1 >= imageWidth ? imageWidth - 1 : x + filterWidth - halfWidth - 1;
+    int y_img_start = y - halfWidth < 0 ? 0 : y - halfWidth;
+    int y_img_end = y + filterWidth - halfWidth - 1 >= imageHeight ? imageHeight - 1 : y + filterWidth - halfWidth - 1;
+    int x_filter_start = x - halfWidth < 0 ? halfWidth - x : 0;
+    int x_filter_end = x + filterWidth - halfWidth - 1 >= imageWidth ? filterWidth - 1 - (x + filterWidth - halfWidth - 1 - imageWidth) : filterWidth - 1;
+    int y_filter_start = y - halfWidth < 0 ? halfWidth - y : 0;
+    int y_filter_end = y + filterWidth - halfWidth - 1 >= imageHeight ? filterWidth - 1 - (y + filterWidth - halfWidth - 1 - imageHeight) : filterWidth - 1;
+
+    for(int j = y_img_start; j <= y_img_end; j++){
+        int filter_index = (j - y_img_start + y_filter_start) * filterWidth;
+        int image_index = j * imageWidth;
+        for(int i = x_img_start; i <= x_img_end; i++){
+            if(!filter[i - x_img_start + x_filter_start + filter_index]){
+                continue;
+            }
+            sum += filter[i - x_img_start + x_filter_start + filter_index] * inputImage[image_index + i];
+        }
+    }
+
+    // for(int i = 0; i < filterWidth; i++){
+    //     int xIndex = x + i - halfWidth;
+    //     for(int j = 0; j < filterWidth; j++){
+    //         int yIndex = y + j - halfWidth;
+    //         if(xIndex >= 0 && xIndex < imageWidth && yIndex >= 0 && yIndex < imageHeight){
+    //             sum += filter[i + filterWidth * j] * inputImage[yIndex * imageWidth + xIndex];
     //         }
     //     }
     // }
 
-    for(int i = 0; i < filterWidth; i++){
-        int xIndex = x + i - halfWidth;
-        for(int j = 0; j < filterWidth; j++){
-            int yIndex = y + j - halfWidth;
-            if(xIndex >= 0 && xIndex < imageWidth && yIndex >= 0 && yIndex < imageHeight){
-                sum += filter[i + filterWidth * j] * inputImage[yIndex * imageWidth + xIndex];
-            }
-        }
-    }
     outputImage[(y) * imageWidth + (x)] = sum;
 }
